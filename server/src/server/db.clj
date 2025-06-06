b45ca5b7ac57113a85e13b18df1aa25e7c734253
Magoninho <magoninhogamer@gmail.com>
2025-06-06 10:31:20 -0300

    added date and hour

--- server/src/server/db.clj
+++ server/src/server/db.clj
@@ -2,13 +2,19 @@
 
 (defonce registros (atom {}))
 (defonce next-id (atom 0))
+;; (defn now [] (new java.util.Date))
+(defn now []
+  (.format (java.text.SimpleDateFormat. "MM/dd/yyyy") (new java.util.Date)))
+
+(defn hour-now []
+  (.format (java.text.SimpleDateFormat. "HH:mm") (new java.util.Date)))
 
 (defn limpar []
   (reset! registros []))
 
 (defn adicionar-registro [nome calorias]
     (let [id (swap! next-id inc)]
-        (swap! registros assoc id {:id id :nome nome :calorias calorias})))
+        (swap! registros assoc id {:id id :created_at (now) :nome nome :calorias calorias})))
 
 (defn get-registros []
     @registros)
